fc3adfbd06824a3c3a4273e4f960bedcaf5d50b6
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-02-20 10:16:08 -0500

    updates constructor to set the name attribute

--- character.py
+++ character.py
@@ -4,4 +4,8 @@
 # inventory
 # 
 class Character():
-    pass
+    # "dunder init"
+    def __init__(self, new_name):
+        # `self` is the customary way to refer to the instance being built
+        # in other languages, they use `this`
+        self.name = new_name
